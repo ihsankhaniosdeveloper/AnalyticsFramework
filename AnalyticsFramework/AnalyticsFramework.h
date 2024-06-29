b83be9f4d15938cda79ee4e8727c5b595524e1c4
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2024-06-30 01:56:59 +0400

    Initial Commit

---
+++ AnalyticsFramework/AnalyticsFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticsFramework.h
+//  AnalyticsFramework
+//
+//  Created by Ahmed Durrani on 30/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticsFramework.
+FOUNDATION_EXPORT double AnalyticsFrameworkVersionNumber;
+
+//! Project version string for AnalyticsFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsFramework/PublicHeader.h>
+
+
