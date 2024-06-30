b42d0d33dcf9478a013cbad4760fe47457d41620
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2024-06-30 16:57:26 +0400

    Fix soure_files path in podspec

---
+++ Sources/AnalyticsFramework/AnalyticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsManager.swift
+//  AnalyticsFramework
+//
+//  Created by Ahmed Durrani on 30/06/2024.
+//
+
+import Foundation
---
+++ Tests/AnalyticsFrameworkTests/AnalyticsFrameworkTests.swift.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsFrameworkTests.swift.swift
+//  AnalyticsFramework
+//
+//  Created by Ahmed Durrani on 30/06/2024.
+//
+
+import Foundation
